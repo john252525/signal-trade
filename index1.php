a4de4f3298d431a1c1db769637011466c228e5c8
root <root@obs.test>
2023-10-27 00:38:36 +0300

    index1.php

--- index1.php
+++ index1.php
@@ -46,27 +46,12 @@ function httprequest($url){
 	$db = new SafeMySQL($opts); // with some of the default settings overwritten
 
 
-	if(isset($_GET['provider_id'])){
-		require_once 'lib/Providers.class.php';
-		$provider = new Providers($db, _DB_TABLE_PROVIDER_); 
-
-		$provider_id=intval($_GET['provider_id']);		
-		$url=$provider->GetUrl($provider_id);
-
-		if ($url==false){
-			die ('Error: provider unknown');
-		}
-	}else{
-		die ('Error: need provider id');
-	}
-
-
 	if(isset($_GET['token'])){
 
 		require_once 'lib/Users.class.php';
 		$user = new Users($db, _DB_TABLE_USER_); 
 
-		$token=intval($_GET['token']);		
+		$token=$_GET['token'];		
 		$user_id=$user->GetUserId($token);
 
 		if ($user_id==false){
@@ -76,7 +61,27 @@ function httprequest($url){
 		die ('Error: need user token');
 	}
 
-	if(isset($_GET['text'])){
+
+	if(isset($_GET['provider'])){
+		require_once 'lib/Providers.class.php';
+		$provider = new Providers($db, _DB_TABLE_PROVIDER_); 
+
+		$provider_name=$_GET['provider'];
+		$provider_data=$provider->GetUrl($provider_name);
+
+		if ($provider_data==false){
+			die ('Error: provider unknown');
+		}
+
+		$provider_id=$provider_data[0]['id'];
+		$provider_url=$provider_data[0]['url'];
+
+	}else{
+		die ('Error: need provider name');
+	}
+
+
+	if ( (isset($_GET['text'])) && (trim($_GET['text'])!='') ){
 
 		$text_in=$_GET['text'];
 		// $text_in='LONG #SOLUSDT from $24.83 stop loss $24.73';
@@ -87,21 +92,21 @@ function httprequest($url){
 		echo 'input_data:'.$text_in.'<br>';
 
 		// Сохранить вводный пресигнал от бота
-		$presignal_id=$presignal->SavePresignal($text_in, $provider_id, $user_id, _DB_TABLE_PRESIGNAL_);
+		$presignal_id=$presignal->SavePresignal ( $text_in, $provider_id, $user_id, _DB_TABLE_PRESIGNAL_ );
 
 		// Получить расшифровку из облака
-		$text_get=httprequest($url . rawurlencode($text_in));
+		$text_get=httprequest( $provider_url . rawurlencode($text_in) );
 		echo 'get_data:'.$text_get.'<br>';
 
 		// Сохранить расшифровку в БД в строку с пресигналом от бота
-		$presignal->UpdatePresignal($text_get, $presignal_id, _DB_TABLE_PRESIGNAL_);
+		$presignal->UpdatePresignal( $text_get, $presignal_id, _DB_TABLE_PRESIGNAL_ );
 
 		require_once 'lib/Signal.class.php';
 		$signal = new Signal($db); 
 
 		// Добавить в расшифровку недостающие данные (takeprofit, stoploss и пр.)
 		// Кол-во готовых сигналов зависит от кол-ва правил постащика
-		$fullsignals=$signal->CalculateSignals($text_get, $provider_id, _DB_TABLE_RULE_);
+		$fullsignals=$signal->CalculateSignals( $text_get, $provider_id, _DB_TABLE_RULE_ );
 		echo "<hr><pre>"; print_r($fullsignals); echo "</pre>"; 
 
 		require_once 'lib/Order.class.php';
@@ -109,13 +114,13 @@ function httprequest($url){
 
 		foreach ($fullsignals as $rule_id => $fullsignal) {
 			// Сохранить сигнал в БД
-			$signal_id=$signal->SaveSignal($fullsignal, $presignal_id, $rule_id, _DB_TABLE_SIGNAL_);	
+			$signal_id=$signal->SaveSignal( $fullsignal, $presignal_id, $rule_id, _DB_TABLE_SIGNAL_ );	
 
 			// Создать ордера (1 на покупку/продажу и несколько на продажу/покупку)
 			$orders=$order->CalcOrder($fullsignal);
 			foreach ($orders as $orderdata) {			
 				// Сохранить ордер в БД
-				$order->SaveOrder($orderdata, $signal_id, _DB_TABLE_ORDER_);	
+				$order->SaveOrder( $orderdata, $signal_id, _DB_TABLE_ORDER_ );	
 				echo "<hr><pre>"; print_r($orderdata); echo "</pre>"; 
 			}
 		}
